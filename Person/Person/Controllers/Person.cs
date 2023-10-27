542547039b756a959a99c7077b0ea7173504be0e
thuha1908 <you@example.com>
2023-10-27 23:04:45 +0700

    Person

---
+++ Person/Person/Controllers/Person.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Person.Controllers
+{
+    public class Person : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Index(string soA, string soB, string soC)
+        {
+           
+          
+            return View();
+        }
+    }
+}
